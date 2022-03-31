39b1c93d1c1e51fd19044c25dd739ea408a645b0
Donguk <95donguk.dev@gmail.com>
2022-03-31 18:38:19 +0900

    confilict2

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("수식을 입력해주세요(예> 1 + 2) : ");
+	printf("수식을 입력해주세요() : ( 2 - 1)");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
@@ -39,22 +39,22 @@ int main(int argc, char* argv[])
  
 int add(int number1, int number2)
 {
-	return number1 + number2
+	//TODO implementation
+	return 0;
 }
  
 int sub(int number1, int number2)
 {
-	return number1 - number2
+	//TODO implementation
+	return 0;
 }
  
 int multi(int number1, int number2)
 {
-	//TODO implementation
 	return number1 * number2;
 }
  
 int div(int number1, int number2)
 {
-	//TODO implementation
 	return (int) number1 / number2;
 }
